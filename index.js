381c92a0309143a7bae9cbdb620688305a66a5f6
jangrubin <wkdfnqls2465@gmail.com>
2023-04-28 11:00:52 +0900

    prompt 입력 여러번 받게하기

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const test = inquirer.prompt([{
   type : "input",
   // name 문자열로 해야 함
   name : "htmlFileName",
-  message : "html 파일의 이름을 정해주세요 : "
+  message : "html 파일의 이름을 정해주세요 :",
+},
+{
+  type : "input",
+  name : "title",
+  message : "html 파일의 제목을 입력하세요 :"
 }]);
 test.then((result)=> {
   // console.log(result);
