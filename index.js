791910090d9049db77654d58d6008dfec18dcbc6
jangrubin <wkdfnqls2465@gmail.com>
2023-04-28 10:32:48 +0900

    입력 값 받아오기

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ import {program} from "commander"
 
 const test = inquirer.prompt([{
   type : "input",
-  name : "입력해봐",
-  message : ""
+  // name 문자열로 해야 함
+  name : "htmlFileName",
+  message : "html 파일의 이름을 정해주세요 : "
 }]);
 test.then((result)=> {
-  console.log(result);
+  // console.log(result);
+  const htmlFileName = result.htmlFileName;
+  console.log(htmlFileName);
 })
