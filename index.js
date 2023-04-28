2c5c275fe6595d9d607ae1fdfa5b0828aab99f20
jangrubin <wkdfnqls2465@gmail.com>
2023-04-28 10:26:19 +0900

    inquirer 테스트

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ import {program} from "commander"
 // ! commander라는 이름을 지원하지 않음
 // console.log(program);
 // console.log(inquirer);
+
+const test = inquirer.prompt([{
+  type : "input",
+  name : "입력해봐",
+  message : ""
+}]);
+test.then((result)=> {
+  console.log(result);
+})
